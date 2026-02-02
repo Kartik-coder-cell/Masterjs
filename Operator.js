36ec485e07fa3109133f15fa849bc64bc247879b
Kartik-coder-cell <gharatekartik45@gmail.com>
2026-02-03 00:20:49 +0530

    Js operators

---
+++ Operator.js
@@ -0,0 +1,23 @@
+//arithemetic operators
+   //+ - * / % ++ --
+
+//assignment operators
+   //= += -= *= /= %=
+
+//comparison operators
+   //== === != !== > < >= <=
+
+//logical operators
+   //&& || !
+   
+//bitwise operators
+   //& | ^ ~ << >> >>>  
+
+//ternary operator
+    //condition ? expr1 : expr2
+
+//typeof operator
+    //typeof variable
+
+//delete operator
+    //delete object.property
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Trying to master js <br>
 1.Know thw concept of let var and const <br>
 2.data types - primitives and non-primitives<br>
+3.js opearators
