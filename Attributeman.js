7b8c91b06ad1e8053a77ccf178265100bc2e38f6
Kartik-coder-cell <gharatekartik45@gmail.com>
2026-02-17 09:14:45 +0530

    Understanding of Attribute manipulation in js

---
+++ Attributeman.js
@@ -0,0 +1,22 @@
+// 1. setattribute
+//setAttribute() method is used to set the value of an attribute on the specified element. If the attribute already exists, the value is updated; otherwise, a new attribute is added with the specified name and value.
+//syntax - setAttribute(attributeName, attributeValue);
+
+// let abc = document.getElementById("a");
+// abc.setAttribute("href", "https://www.google.com");
+
+// 2. getattribute
+//getAttribute() method is used to get the value of an attribute on the specified element. If the attribute does not exist, it returns null.
+//syntax - getAttribute(attributeName);     
+
+// let a = document.getElementById("a");
+// let hrefValue = a.getAttribute("href");
+// console.log(hrefValue); // Output: https://www.google.com
+
+// 3. removeattribute
+//removeAttribute() method is used to remove an attribute from the specified element. If the attribute does not exist, it does nothing.
+//syntax - removeAttribute(attributeName);  
+
+// let a = document.getElementById("a");
+// a.removeAttribute("href");
+
