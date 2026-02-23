41f1fd4eafbf82cc5f517fd6c50b3ab4ea677263
Kartik-coder-cell <gharatekartik45@gmail.com>
2026-02-23 15:36:51 +0530

    unserstanding the concept of timers and intervals

--- README.md
+++ README.md
@@ -14,3 +14,4 @@ Trying to master js <br>
 12.Classlist<br>
 13.Event and Eventlistener and Event object<br>
 14.Event bubbling and capturing<br>
+15.Timers and counters<br>
---
+++ TimersandIntervals.js
@@ -0,0 +1,23 @@
+//1.settimeout - runs a function after a specified number of milliseconds)
+
+// setTimeout(function(){
+//     console.log("Hii kartik");
+// }, 5000);
+
+//2.setinterval - in setinterval code repeatedly runs after mentioned time period 
+
+// setInterval(function(){
+//     console.log("Hii kartik jii");
+// }, 5000);
+
+//3.cleartimeout - clear it = no output come 
+
+// clearTimeout(function(){
+//     console.log("Hii");
+// } , 4000);
+
+//4.clearinterval - same as cleartimeout 
+
+clearInterval(function(){
+     console.log("Nothing to print and show ");
+}, 2000);
